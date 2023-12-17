131298c1202758a8de12c8d27c07bd78aa9ef00a
Dmitrijs <graudu_12@yahoo.co.uk>
2023-12-17 01:23:58 +0000

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,22 @@
+//function getRandomHexColor() {
+ // return `#${Math.floor(Math.random() * 16777215)
+ //   .toString(16)
+ //   .padStart(6, 0)}`;
+//}
+const changeColorBtn = document.querySelector('button.change-color');
+const body = document.body;
+const colorValue = document.querySelector('span.color');
+
+changeColorBtn.addEventListener('click', onChangeColorButton);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function onChangeColorButton() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
 }
