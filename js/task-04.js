b09100de2b86d06316cc4c38bc2cf849e5cafae7
Dmitrijs <graudu_12@yahoo.co.uk>
2023-12-17 01:00:49 +0000

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.target.elements;
+
+  if (!email.value || !password.value) {
+    alert('All form fields must be filled in');
+  }
+
+  const userDetails = { email: email.value, password: password.value };
+  //console.log(userDetails);
+
+  event.target.reset();
+}
