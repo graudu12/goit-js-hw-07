3c9e659a97145bc1f72f72daf2dc03ea4b13ac80
Dmitrijs <graudu_12@yahoo.co.uk>
2023-12-19 10:10:03 +0000

    upd-hw-3-6

--- js/task-03.js
+++ js/task-03.js
@@ -1,8 +1,8 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
-nameInput.addEventListener('input', onInputChange);
-
-function onInputChange({ target: { value } }) {
-  nameOutput.textContent = value || 'Anonymous';
-}
+nameInput.addEventListener('input', event => {
+  nameOutput.textContent = event.currentTarget.value.trim()
+    ? event.currentTarget.value
+    : 'Anonymous';
+});
--- js/task-06.js
+++ js/task-06.js
@@ -18,10 +18,10 @@ function createBoxes(amount) {
     divBox.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
     boxes.insertAdjacentElement('beforeend', divBox);
   }
-  formInput.value = '';
+  formInput.value = Number('');
 }
 btnCreate.addEventListener('click', () => {
-  if (1 <= formInput.value <= 100) {
+  if (1 >= formInput.value <= 100) {
     createBoxes(formInput.value);
   }
 });
